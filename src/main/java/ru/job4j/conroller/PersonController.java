ff847b915f19f9c451f7c7bbc498be0f985ea44d
beloc <belochuvach@gmail.com>
2023-07-28 12:29:01 +0300

    Обработка исключений и Spring REST

--- src/main/java/ru/job4j/conroller/PersonController.java
+++ src/main/java/ru/job4j/conroller/PersonController.java
@@ -1,18 +1,31 @@
 package ru.job4j.conroller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import org.hibernate.exception.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.model.Person;
 import ru.job4j.service.PersonService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
 @RequestMapping("/person")
 @AllArgsConstructor
 public class PersonController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersonController.class.getSimpleName());
+
+    private final ObjectMapper objectMapper;
 
     private final PersonService persons;
 
@@ -22,29 +35,28 @@ public class PersonController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Person> findById(@PathVariable int id) {
-        var person = persons.findById(id);
-        return new ResponseEntity<>(
-                person.orElse(new Person()),
-                person.isPresent() ? HttpStatus.OK : HttpStatus.NOT_FOUND
-        );
+    public Person findById(@PathVariable int id) {
+        return persons.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Person is not found."
+        ));
     }
 
     @PostMapping("/")
-    public ResponseEntity<Person> create(@RequestBody Person person) {
+    public Person create(@RequestBody Person person) {
         var optionalPerson = persons.save(person);
-        return optionalPerson.map(value -> new ResponseEntity<>(
-                value,
-                HttpStatus.CREATED
-        )).orElseGet(() -> new ResponseEntity<>(HttpStatus.CONFLICT));
+        return person;
     }
 
     @PutMapping("/")
     public ResponseEntity<Void> update(@RequestBody Person person) {
-       if (persons.update(person)) {
+        if (person.getPassword().length() == 0 || person.getLogin().length() == 0) {
+            throw new NullPointerException("Password or login is empty");
+        }
+        if (persons.update(person)) {
            return ResponseEntity.ok().build();
-       }
-       return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.badRequest().build();
     }
 
     @DeleteMapping("/{id}")
@@ -53,5 +65,17 @@ public class PersonController {
             return ResponseEntity.ok().build();
         }
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    public void constraintException(Exception e, HttpServletRequest request,
+                                    HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() { {
+            put("message", "Some of fields empty");
+            put("details", e.getMessage());
+        }}));
+        LOGGER.error(e.getMessage());
     }
 }
