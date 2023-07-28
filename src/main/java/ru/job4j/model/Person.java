13af7b52e6c2d0afcd6a2db1fd269ea7de31b943
beloc <belochuvach@gmail.com>
2023-07-28 10:12:51 +0300

    RESTFul. Описание архитектуры

--- src/main/java/ru/job4j/model/Person.java
+++ src/main/java/ru/job4j/model/Person.java
@@ -1,11 +1,9 @@
 package ru.job4j.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
 
 @Entity
 @Data
